0dd7d173ff9c2ed00dc51b57563f9db7b9ceb17a
Mrzhangy <item_group@163.com>
2017-12-21 18:27:30 +0800

    home主体结构

---
+++ src/components/Home.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="home">
+        <el-container>
+            <el-header>Header</el-header>
+            <el-container> 
+                <el-aside>Aside</el-aside>
+                <el-main>Main</el-main>
+            </el-container>
+            <el-footer>Footer</el-footer>
+        </el-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+    body {
+        margin: 0;
+    }
+    .el-header, .el-footer {
+        background-color: #B3C0D1;
+        color: #333;
+        text-align: center;
+        line-height: 60px;
+    }
+  
+    .el-aside {
+        background-color: #D3DCE6;
+        color: #333;
+        text-align: center;
+        line-height: 200px;
+    }
+      
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        text-align: center;
+        min-height: 500px;
+    }
+           
+</style>
